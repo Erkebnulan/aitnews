c1abd64f7c3e6fd8cc45c3de8b195b9b4497bbce
erkebulan <erkebulan.safiolla@mail.ru>
2020-09-22 11:14:17 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+$('.call-aside').click(function (){
+    $('.left-menu').css('transform','translateX(0)');
+});
+
+$('.left-menu-close').click(function (){
+    $('.left-menu').css('transform','translateX(-100%)')
+});
